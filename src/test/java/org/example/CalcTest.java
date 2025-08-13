622e2bd40d52a035244d8968d605b3962fd13459
Shehan Pathirana <25011180@massey.ac.nz>
2025-08-13 18:44:11 +1200

    Move JUnit code to test sources; implement subtract()

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -1,14 +1,20 @@
 package org.example;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalcTest {
-  Calc c = new Calc();
 
     @Test
     void testAddition() {
-        assertEquals(4, c.add(2,2));
+        Calc c = new Calc();
+        assertEquals(7, c.add(3, 4));
     }
 
+    // Add this per the tutorial
+    @Test
+    void testSubtraction() {
+        Calc c = new Calc();
+        assertEquals(2, c.subtract(4, 2));
+    }
 }
